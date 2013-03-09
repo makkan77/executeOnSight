0dd2f1ad7ae9feedcc78111b88247b6520bc02c6
Markus Ulfberg <markus.ulfberg@gmail.com>
2013-03-09 23:56:12 +0100

    First upload of script

---
+++ executeOnSight.sh
@@ -0,0 +1,26 @@
+# /bin/sh
+
+# executeOnSight.sh 
+# Runs desired $ACTION when $FILE is present in $DIR 
+# Shell script by Markus Ulfberg 2013-03-07
+
+# Variables
+# Path where FILE is expected
+DIR=/media/usb-shutdown
+# Name of file too look for 
+FILE=systemHaltNow
+# Action/Task/Command to perfrm 
+ACTION="/sbin/shutdown -h now"
+
+# Check if FILE exists and is not empty. 
+if [ ! -s $DIR/$FILE ]
+	then
+		# FILE does not exist, do Nothing
+		echo "$FILE not present. Carry on!"
+	else
+		# FILE exists, perform ACTION
+		echo "$FILE present, executing $ACTION NOW!"
+		$ACTION
+fi
+
+exit
